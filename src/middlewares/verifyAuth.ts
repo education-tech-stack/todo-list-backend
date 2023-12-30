60e44de82d7eaae90cd71e7aa8a0a4d3cc683e0e
Sameer Chauhan <sameer55chauhan@gmail.com>
2023-12-30 13:22:19 +0530

    fix: changed post method to get & removed Auth header from verifyAuth as its redundant

--- src/middlewares/verifyAuth.ts
+++ src/middlewares/verifyAuth.ts
@@ -5,14 +5,13 @@ const User = db.User
 
 export default function verifyToken(req, res, next) {
   const token = req.cookies.jwt
-  const authHeader = req.headers['Authorization']
 
-  if (!token && !authHeader) {
+  if (!token) {
     return res.status(401).json({ error: 'No token provided' })
   }
 
   try {
-    const decoded = jwt.verify(token ? token: authHeader.split(' '[1]), process.env.SECRET_KEY)
+    const decoded = jwt.verify(token, process.env.SECRET_KEY)
     User.findByPk(decoded.id).then((user) => {
       req.user_id = user.id
       next()
