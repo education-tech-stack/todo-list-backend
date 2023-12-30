8262d9fadb617a95fc0fcfea7aa04fe3e538969f
Sameer Chauhan <sameer55chauhan@gmail.com>
2023-12-30 15:12:40 +0530

    feat: added secret token for admin

--- src/middlewares/verifyAuth.ts
+++ src/middlewares/verifyAuth.ts
@@ -5,17 +5,23 @@ const User = db.User
 
 export default function verifyToken(req, res, next) {
   const token = req.cookies.jwt
+  const authHeader = req.headers['Authorization']
 
-  if (!token) {
+  if (!token && !authHeader) {
     return res.status(401).json({ error: 'No token provided' })
   }
 
   try {
     const decoded = jwt.verify(token, process.env.SECRET_KEY)
-    User.findByPk(decoded.id).then((user) => {
-      req.user_id = user.id
+    if (authHeader == process.env.AUTH_TOKEN) {
+      req.user_id = 1
       next()
-    })
+    } else {
+      User.findByPk(decoded.id).then((user) => {
+        req.user_id = user.id
+        next()
+      })
+    }
   } catch (err) {
     return res.status(401).json({ error: 'Invalid token' })
   }
