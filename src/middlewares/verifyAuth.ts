d404ae44ebeb053017b4445c503942e2794dbd34
Sameer Chauhan <sameer55chauhan@gmail.com>
2023-12-31 01:37:10 +0530

    feat: provision for admin token

--- src/middlewares/verifyAuth.ts
+++ src/middlewares/verifyAuth.ts
@@ -5,23 +5,26 @@ const User = db.User
 
 export default function verifyToken(req, res, next) {
   const token = req.cookies.jwt
-  const authHeader = req.headers['Authorization']
+  const authHeader = req.headers['authorization']
 
   if (!token && !authHeader) {
     return res.status(401).json({ error: 'No token provided' })
   }
 
   try {
-    const decoded = jwt.verify(token, process.env.SECRET_KEY)
-    if (authHeader == process.env.AUTH_TOKEN) {
+    if (authHeader) {
+      if (authHeader !== process.env.AUTH_TOKEN)
+        throw Error('Invalid Auth token')
       req.user_id = 1
       next()
-    } else {
-      User.findByPk(decoded.id).then((user) => {
-        req.user_id = user.id
-        next()
-      })
+      return;
     }
+
+    const decoded = jwt.verify(token, process.env.SECRET_KEY)
+    User.findByPk(decoded.id).then((user) => {
+      req.user_id = user.id
+      next()
+    })
   } catch (err) {
     return res.status(401).json({ error: 'Invalid token' })
   }
