83117d7d76da15004ec6c48fdeeca78d6e911abd
Sameer Chauhan <sameer55chauhan@gmail.com>
2023-12-30 14:08:32 +0530

    feat: check if auth works now or not

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,48 +1,53 @@
-import bcrypt from "bcrypt";
-import db from "../models";
-import jwt from "jsonwebtoken";
+import bcrypt from 'bcrypt'
+import db from '../models'
+import jwt from 'jsonwebtoken'
 
-const User = db.User;
+const User = db.User
 
 export async function signup(req, res) {
   try {
-    const { userName, email, password } = req.body;
+    const { userName, email, password } = req.body
     const data = {
       userName,
       email,
       password: await bcrypt.hash(password, 10),
-    };
-    
-    const user = await User.create(data);    
+    }
+
+    const user = await User.create(data)
 
     if (user) {
       let token = jwt.sign({ id: user.id }, process.env.SECRET_KEY, {
         expiresIn: 1 * 24 * 60 * 60 * 1000,
       })
 
-      res.cookie("jwt", token, { maxAge: 1 * 24 * 60 * 60, httpOnly: true, sameSite: 'strict' });
-      console.log("user", JSON.stringify(user, null, 2));
-      return res.status(201).send({ ...user, "access_token": token });
+      res.cookie('jwt', token, {
+        maxAge: 1 * 24 * 60 * 60,
+        httpOnly: true,
+        secure: true,
+        sameSite: 'none',
+      })
+      console.log('user', JSON.stringify(user, null, 2))
+      return res.status(201).send({ ...user, access_token: token })
     } else {
-      return res.status(409).send("Details are not correct");
+      return res.status(409).send('Details are not correct')
     }
   } catch (error) {
-    console.log(error);
+    console.log(error)
   }
-};
+}
 
 export async function login(req, res) {
   try {
-    const { email, password } = req.body;
+    const { email, password } = req.body
 
     const user = await User.findOne({
       where: {
         email: email,
       },
-    });
+    })
 
     if (user) {
-      const isSame = await bcrypt.compare(password, user.password);
+      const isSame = await bcrypt.compare(password, user.password)
 
       if (isSame) {
         let token = jwt.sign({ id: user.id }, process.env.SECRET_KEY, {
@@ -52,20 +57,20 @@ export async function login(req, res) {
         res.cookie('jwt', token, {
           maxAge: 1 * 24 * 60 * 60 * 1000,
           httpOnly: true,
-          sameSite: 'strict',
+          secure: true,
+          sameSite: 'none',
         })
         // console.log("user", JSON.stringify(user, null, 2));
         // console.log(token);
 
         return res.status(201).send({ ...user, access_token: token })
       } else {
-        return res.status(401).send("Authentication failed");
+        return res.status(401).send('Authentication failed')
       }
     } else {
-      return res.status(401).send("Authentication failed");
+      return res.status(401).send('Authentication failed')
     }
   } catch (error) {
-    console.log(error);
+    console.log(error)
   }
-};
-
+}
